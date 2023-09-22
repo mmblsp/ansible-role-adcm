74d948a60ae786928b9022f400b9c84965af7d42
anatoly <anatoly@mmblspace.ru>
2023-09-22 06:17:52 +0300

    v0.1.0

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,5 +1,6 @@
 """Test ADCM server."""
 import os
+import time
 
 import testinfra.utils.ansible_runner
 
@@ -9,12 +10,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_adcm_value(host):
+    """Test adcm storage."""
     fle = host.file('/opt/adcm')
     assert fle.is_directory
 
 
 def test_adcm_service(host):
     """Test adcm service."""
-    output = host.check_output('curl http://localhost:8000')
-    assert 'Arenadata Cluster Manager' in output
+    for _ in range(30):
+        try:
+            output = host.check_output('curl http://localhost:8000')
+            break
+        except AssertionError:
+            time.sleep(1)
     assert host.socket('tcp://0.0.0.0:8000').is_listening
+    assert 'Arenadata Cluster Manager' in output
